a90c9655a07d38823dc10caeb1cda57b4ad08ba5
Thomas Joußen <tjoussen@databay.de>
2020-02-27 11:04:27 +0100

    Add rollup for build process

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  external: [
+      'superagent',
+      'querystring'
+  ],
+  plugins: [
+    external(),
+    url(),
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs()
+  ]
+};
